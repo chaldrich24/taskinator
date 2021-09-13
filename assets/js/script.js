41b44e9eba078af37089534aac7ea836ef2b7b84
Snake <chaldrich24@gmail.com>
2021-09-13 11:44:57 -0400

    add validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,22 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
 
     // package data into an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
+
     createTaskEl(taskDataObj);
-    
 }
 
 var createTaskEl = function(taskDataObj) {
